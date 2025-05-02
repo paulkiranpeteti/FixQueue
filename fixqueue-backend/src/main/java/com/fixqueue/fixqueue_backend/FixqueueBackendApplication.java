243bf8bb4a83e60a54efcb8989600bfdd0527da6
petetipaulkiran <petetipaulkiran@gmail.com>
2025-05-02 11:35:56 +0530

    Created the Folder Structures for both frontend and backend to implement the Project

---
+++ fixqueue-backend/src/main/java/com/fixqueue/fixqueue_backend/FixqueueBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fixqueue.fixqueue_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixqueueBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixqueueBackendApplication.class, args);
+	}
+
+}
---
+++ fixqueue-backend/src/test/java/com/fixqueue/fixqueue_backend/FixqueueBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fixqueue.fixqueue_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixqueueBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
